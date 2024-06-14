84c023de6b567850b4c31910d057953f40563e0b
vivekmen <vivek.menapara2510@gmail.com>
2024-06-14 18:53:50 +0530

    creat player

--- index.js
+++ index.js
@@ -6,12 +6,29 @@ const server=http.createServer(app);
 const {Server}=require("socket.io");
 const io=new Server(server)
 
-const default_turn="w";
+let player={};
+const current_player="w";
+
 
 io.on('connection',(socket)=>{
   console.log("user connected");
+ 
+     if(!player.white){
+      player.white=socket.id;
+      socket.emit("playerRole","w")
+     }
+     else{
+      player.black=socket.id
+      socket.emit('playerRole','b');
+     }
 
-   
+     socket.on('disconnected',()=>{
+      if(socket.id===player.white){
+        delete player.white;
+      }else if(socket.id===player.black) {
+        delete player.black;
+      }
+     })
 })
 
 app.get("/", (req, res) => {
